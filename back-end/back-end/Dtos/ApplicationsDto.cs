4b82f92b05bba13d15cc7846d9d5a4cbf75685ce
Cheezco <60855273+Cheezco@users.noreply.github.com>
2022-12-18 17:29:18 +0200

    Add dtos

---
+++ back-end/back-end/Dtos/ApplicationsDto.cs
@@ -0,0 +1,7 @@
+﻿namespace back_end.Dtos;
+
+public record ApplicationDto(int Id, AplicationState State, DateTime? Date, int JobAdId, int UserId);
+
+public record CreateApplicationDto(int UserId);
+
+public record UpdateApplicationDto(AplicationState State);
